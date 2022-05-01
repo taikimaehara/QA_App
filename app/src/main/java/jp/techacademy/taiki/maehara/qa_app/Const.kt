8f2581db8af36c2b0807f3a7d664b7103b34443e
ktec-ta-maehara <ta-maehara@kddi-tech.com>
2022-05-01 13:46:38 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/taiki/maehara/qa_app/Const.kt
@@ -0,0 +1,13 @@
+package jp.techacademy.taiki.maehara.qa_app
+
+// Firebaseにユーザの表示名を保存するパス
+const val UsersPATH = "users"
+
+// Firebaseに質問を保存するパス
+const val ContentsPATH = "contents"
+
+// Firebaseに回答を保存するパス
+const val AnswersPATH = "answers"
+
+// Preferenceに表示名を保存するときのキー
+const val NameKEY = "name"
